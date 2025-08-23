6708aef6d717c35a5c5356f39162f7b057f983dd
Bill Chickering <chickering.bill@gmail.com>
2025-08-22 18:02:08 -0700

    add ftplugin/lua.lua

---
+++ ftplugin/lua.lua
@@ -0,0 +1,5 @@
+-- Lua-specific indentation settings to match .stylua.toml
+vim.bo.expandtab = true    -- Use spaces instead of tabs
+vim.bo.shiftwidth = 2      -- Number of spaces for indentation
+vim.bo.tabstop = 2         -- Display width of tabs
+vim.bo.softtabstop = 2     -- Number of spaces for tab key
