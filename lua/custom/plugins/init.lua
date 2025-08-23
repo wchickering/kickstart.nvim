3b6f13eaeb88ddb65a3ef40a8867d37eef683df2
Bill Chickering <chickering.bill@gmail.com>
2025-08-23 16:30:13 -0700

    use individual lua files for plugin config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,56 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-
-  {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
-
-    config = function()
-      require('oil').setup()
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-    end,
-  },
-
-  {
-    'nvim-telescope/telescope-file-browser.nvim',
-    event = 'VimEnter',
-    dependencies = {
-      'nvim-telescope/telescope.nvim',
-      'nvim-lua/plenary.nvim',
-    },
-    config = function()
-      local telescope = require 'telescope'
-      local fb_actions = telescope.extensions.file_browser.actions
-
-      telescope.setup {
-        extensions = {
-          file_browser = {
-            mappings = {
-              ['i'] = {
-                -- Only override file open, leave directories alone
-                ['<C-o>'] = fb_actions.open,
-              },
-              ['n'] = {
-                ['<C-o>'] = fb_actions.open,
-              },
-            },
-          },
-        },
-      }
-
-      telescope.load_extension 'file_browser'
-
-      -- open file_browser with the path of the current buffer
-      vim.keymap.set('n', '<space>fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', { desc = '[F]ile [B]rowser' })
-    end,
-  },
-}
+-- 
+-- Individual plugin configurations have been moved to separate files:
+-- - oil.lua
+-- - telescope-file-browser.lua
+--
+-- Add new plugins here or create separate files for them.
+return {}
---
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -0,0 +1,33 @@
+return {
+  'nvim-telescope/telescope-file-browser.nvim',
+  event = 'VimEnter',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    local telescope = require 'telescope'
+    local fb_actions = telescope.extensions.file_browser.actions
+
+    telescope.setup {
+      extensions = {
+        file_browser = {
+          mappings = {
+            ['i'] = {
+              -- Only override file open, leave directories alone
+              ['<C-o>'] = fb_actions.open,
+            },
+            ['n'] = {
+              ['<C-o>'] = fb_actions.open,
+            },
+          },
+        },
+      },
+    }
+
+    telescope.load_extension 'file_browser'
+
+    -- open file_browser with the path of the current buffer
+    vim.keymap.set('n', '<space>fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', { desc = '[F]ile [B]rowser' })
+  end,
+}
